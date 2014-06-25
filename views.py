6046b2f95de75587db27646ecda61ef73fb98e32
Christian Wittwer <wittwerch@gmail.com>
2014-06-25 06:37:17 +0000

    User interface enhanced

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 from services import get_tile_clusters, get_images, get_master_instances
 
 app = Flask(__name__)
@@ -11,6 +11,29 @@ def inject_user():
         username = "Anyonmous"
 
     return dict(username=username)
+
+@app.template_filter('highlight_with_label')
+def highlight_with_label(s):
+    """
+    http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-describing-stacks.html
+    """
+    html = '<span class="label %s">%s</span>'
+    s = s.upper()
+
+    # success
+    if s in ('RUNNING', 'AVAILABLE', 'CREATE_COMPLETE', 'UPDATE_COMPLETE', 'UPDATE_ROLLBACK_COMPLETE'):
+        return Markup(html % ('label-success', s))
+    # info
+    elif s in ('PENDING'):
+        return Markup(html % ('label-info', s))
+    # danger
+    elif s in ('STOPPED'):
+        return Markup(html % ('label-danger', s))
+    else:
+        return Markup(html % ('label-default', s))
+        
+
+    return s[::-1]
 
 @app.route('/')
 def index():

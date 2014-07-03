6dfc91e5b7e732bb9ca46f62b9451bdf4880f68f
Christian Wittwer <wittwerch@gmail.com>
2014-07-03 11:59:30 +0200

    legacy code removed

--- services.py
+++ services.py
@@ -1,8 +1,11 @@
-import config, datetime, json
+import config
+import datetime
+import json
 from boto import ec2, cloudformation
 from boto.ec2 import autoscale, elb, cloudwatch
 from flask import Markup
 from boto.exception import BotoServerError
+
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -11,6 +14,7 @@ class DateTimeEncoder(json.JSONEncoder):
         else:
             encoded_object =json.JSONEncoder.default(self, obj)
         return encoded_object
+
 
 def get_aws_connection(service, region_name='eu-west-1'):
 
@@ -97,87 +101,16 @@ def get_stats(period, start_time, end_time, metric_name, namespace, statistics, 
     stats = json.dumps(data, cls=DateTimeEncoder)
     return Markup(stats)
 
+
 def get_images():
     ec2 = get_aws_connection('ec2')
     filters = {'tag:tcm': 'image'}
     images = ec2.get_all_images(owners=['self'], filters=filters)
     return images
 
+
 def get_master_instances():
     ec2 = get_aws_connection('ec2')
     filters = {'tag:tcm': 'master'}
     return ec2.get_only_instances(filters=filters)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def get_elb_stats(name, metric='RequestCount', minutes=60, period=60):
-    conn = get_cloudwatch_connection()
-    end = datetime.datetime.utcnow()
-    start = end - datetime.timedelta(minutes=minutes)
-    stats = conn.get_metric_statistics(period, start, end, metric, 'AWS/ELB', 'Sum', {"LoadBalancerName": name})
-    return stats
-
-def get_cpu_stats(name, metric='CPUUtilization', minutes=60, period=60):
-    conn = get_cloudwatch_connection()
-    end = datetime.datetime.utcnow()
-    start = end - datetime.timedelta(minutes=minutes)
-    stats = conn.get_metric_statistics(period, start, end, metric, 'AWS/EC2', 'Average', {"AutoScalingGroupName": name})
-    print stats
-    return stats
-
-def get_tile_clusters(stack_id=None):
-    ec2_conn = get_ec2_connection()
-    cf_conn = get_cloudformation_connection()
-    elb_conn = get_elb_connection()
-    as_conn = get_autoscale_connection()
-
-    clusters = []
-    for stack in cf_conn.describe_stacks(stack_id):
-        if stack.stack_status not in ('ROLLBACK_COMPLETE'):
-            cluster = {'stack': stack}
-            for resource in stack.list_resources():
-                if resource.resource_type == 'AWS::ElasticLoadBalancing::LoadBalancer':
-                    cluster['elb'] = elb_conn.get_all_load_balancers(load_balancer_names=[resource.physical_resource_id])[0]
-                elif resource.resource_type == 'AWS::AutoScaling::LaunchConfiguration':
-                    kwargs = {'names': [resource.physical_resource_id]}
-                    cluster['launch_config'] = as_conn.get_all_launch_configurations(**kwargs)[0]
-                elif  resource.resource_type == 'AWS::AutoScaling::AutoScalingGroup':
-                    cluster['group'] = as_conn.get_all_groups(names=[resource.physical_resource_id])[0]
-
-            instance_ids = [i.instance_id for i in cluster['group'].instances]
-            cluster['instances'] = ec2_conn.get_only_instances(instance_ids=instance_ids)
-
-            cluster['elb_stats'] = Markup(json.dumps(get_elb_stats(cluster['elb'].name), cls=DateTimeEncoder))
-            cluster['cpu_stats'] = Markup(json.dumps(get_cpu_stats(cluster['group'].name), cls=DateTimeEncoder))
-
-
-            clusters.append(cluster)
-        
-    return clusters
-
-
-
 
